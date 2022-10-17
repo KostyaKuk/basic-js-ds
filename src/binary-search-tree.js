f55f4779a9b816da4c1ca5ec9018a0d89af64581
Konstantin Kukushkin <k.k.o.lukoml@mail.ru>
2022-10-18 02:03:06 +0300

    init:solutions task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,27 +7,72 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor(data = null) {
+    this.rootNode = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.rootNode = addInside(this.rootNode, data);
+
+    function addInside(node, data) {
+      if (!node) {
+        return new Node(data)
+      }
+
+      if (node.data === data) {
+        return node;
+      }
+
+      if (data < node.data) {
+        node.left = addInside(node.left, data);
+      } else {
+        node.right = addInside(node.right, data);
+      }
+
+      return node;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return findInside(this.rootNode, data);
+
+    function findInside(node, data) {
+      if (!node) {
+        return false
+      }
+
+      if (node.data === data) {
+        return true;
+      }
+
+      return data < node.data ?
+        findInside(node.left, data) :
+        findInside(node.right, data);
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  find(data) {
+    return findNode(this.rootNode, data);
 
+    function findNode(node, data) {
+      if (!node) {
+        return null;
+      }
+
+      if (node.data === data) {
+        return node;
+      }
+
+      return data < node.data ?
+        findNode(node.left, data) :
+        findNode(node.right, data);
+    }
+  }
+  
   remove(/* data */) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
